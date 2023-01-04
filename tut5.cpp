ae5a3fc5952ae342ae63b2378685270d0974c357
Ravi Deepak Thakur <101028751+ravithakur-projects@users.noreply.github.com>
2023-01-04 21:34:13 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+// There are two type of header files
+using namespace std;
+
+int main(){
+    
+    int a = 5, b = 4;
+    cout<<"This is hello world"<<endl;
+    cout<<"following are the types of operators in c++"<<endl;
+    //Arithmetic operators
+    cout<<"The value of a + b is "<<a+b<<endl;
+    cout<<"The value of a - b is "<<a-b<<endl;
+    cout<<"The value of a * b is "<<a*b<<endl;
+    cout<<"The value of a / b is "<<a/b<<endl;
+    cout<<"The value of a % b is "<<a%b<<endl;
+    cout<<"The value of a ++ is "<<a++<<endl;
+    cout<<"The value of a -- is "<<a--<<endl;
+    cout<<"The value of ++ a is "<<++a<<endl;
+    cout<<"The value of -- a is "<<--a<<endl;
+    return 0;
+
+}
