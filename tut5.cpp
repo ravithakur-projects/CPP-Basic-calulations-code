b7840df347f262a07384670baa1c4a7348f5ee7d
Ravi Deepak Thakur <101028751+ravithakur-projects@users.noreply.github.com>
2023-01-04 21:49:21 +0530

    Add files via upload

--- tut5.cpp
+++ tut5.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    
+
     int a = 5, b = 4;
     cout<<"This is hello world"<<endl;
     cout<<"following are the types of operators in c++"<<endl;
@@ -17,6 +17,20 @@ int main(){
     cout<<"The value of a -- is "<<a--<<endl;
     cout<<"The value of ++ a is "<<++a<<endl;
     cout<<"The value of -- a is "<<--a<<endl;
+    //Assignment operators --> used to assign values to variables
+    //Comparison operator
+    cout<<"The value of a == b is "<<(a==b)<<endl;
+    cout<<"The value of a != b is "<<(a!=b)<<endl;
+    cout<<"The value of a >= b is "<<(a>=b)<<endl;
+    cout<<"The value of a <= b is "<<(a<=b)<<endl;
+    cout<<"The value of a > b is "<<(a>b)<<endl;
+    cout<<"The value of a < b is "<<(a<b)<<endl;
+    //Logical operator
+    cout<<"Following are the logical operator in C++"<<endl;
+    cout<<"The value of this logical and operator ((a==b) && (a<b)) is:"<<((a==b) && (a<b))<<endl;
+    cout<<"The value of this logical or operator ((a==b) || (a<b)) is:"<<((a==b) || (a<b))<<endl;
+    cout<<"The value of this logical not operator (!(a==b)) is:"<<(!(a==b))<<endl;
+
     return 0;
 
 }
